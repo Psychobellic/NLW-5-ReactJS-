5a557d075a86739c1d57b363fd89bce88914a506
Matheus Schiffer <matheusschiffer@gmail.com>
2021-04-25 17:22:06 -0300

    aula 4 final

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,52 @@
+import { useState } from "react";
 import Header from "../components/Header/index";
 import Player from "../components/Player/index";
+
+import { PlayerContext } from "../contexts/PlayerContext";
 
 import "../styles/global.scss";
 
 import styles from "../styles/app.module.scss";
 
 function MyApp({ Component, pageProps }) {
+	const [episodeList, setEpisodeList] = useState([]);
+	const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+	const [isPlaying, setIsPlaying] = useState(false);
+
+	function play(episode) {
+		setEpisodeList([episode]);
+		setCurrentEpisodeIndex(0);
+		setIsPlaying(true);
+	}
+
+	function togglePlay() {
+		setIsPlaying(!isPlaying);
+	}
+
+	function setPlayingState(state: boolean) {
+		setIsPlaying(state);
+	}
+
 	return (
-		<div className={styles.wrapper}>
-			<main>
-				<Header />
-				<Component {...pageProps} />
-			</main>
-			<div className={styles.playerDiv}>
-				<Player />
+		<PlayerContext.Provider
+			value={{
+				episodeList,
+				currentEpisodeIndex,
+				play,
+				isPlaying,
+				togglePlay,
+				setPlayingState,
+			}}>
+			<div className={styles.wrapper}>
+				<main>
+					<Header />
+					<Component {...pageProps} />
+				</main>
+				<div className={styles.playerDiv}>
+					<Player />
+				</div>
 			</div>
-		</div>
+		</PlayerContext.Provider>
 	);
 }
 
--- src/pages/episode/[slug].tsx
+++ src/pages/episode/[slug].tsx
@@ -3,6 +3,7 @@ import { api } from "../../services/api";
 import { format, parseISO } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { convertDurationToTimeString } from "../../utils/convertDurationToString";
 import Image from "next/image";
 
@@ -25,6 +26,10 @@ type EpisodeProps = {
 };
 
 export default function Episode({ episode }: EpisodeProps) {
+	const router = useRouter();
+	if (router.isFallback) {
+		return <p>Carregando...</p>;
+	}
 	return (
 		<div className={styles.episode}>
 			<div className={styles.thumbnailContainer}>
@@ -62,8 +67,30 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+	const { data } = await api.get("episodes", {
+		params: {
+			_limit: 2,
+			_order: "desc",
+			_sort: "published_at",
+		},
+	});
+
+	const paths = data.map((episode) => {
+		return {
+			params: {
+				slug: episode.id,
+			},
+		};
+	});
+
 	return {
-		paths: [],
+		paths: [
+			{
+				params: {
+					slug: "a-importancia-da-contribuicao-em-open-source",
+				},
+			},
+		],
 		fallback: "blocking",
 	};
 };
