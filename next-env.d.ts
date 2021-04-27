7ae380253d72a5543c2c9181611cc414d4decf58
Matheus Schiffer <matheusschiffer@gmail.com>
2021-04-27 10:22:37 -0300

    reinstallation NextJs after machine cleanup

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-=======
-/// <reference types="next" />
-/// <reference types="next/types/global" />
->>>>>>> 3fc38148f1cae1d1675f6f7a9498dc16458dba56
